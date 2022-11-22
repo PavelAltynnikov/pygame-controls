7903d7c137f6264770f5eb135933dbfa0b3cf57d
PavelAltynnikov <Robenzon11@gmail.com>
2022-11-22 19:21:31 +0500

    add settings module

---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class UserControlSettings:
+    def __init__(self):
+        self.move_right = pygame.K_RIGHT
+        self.move_up = pygame.K_UP
+        self.move_left = pygame.K_LEFT
+        self.move_down = pygame.K_DOWN
