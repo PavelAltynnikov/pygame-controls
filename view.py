fe66d7ea718c1768a10978dc54f9a7bbeb761cce
PavelAltynnikov <Robenzon11@gmail.com>
2022-12-25 21:55:21 +0600

    move Character class to view module

--- view.py
+++ view.py
@@ -2,7 +2,42 @@ from abc import ABC, abstractmethod
 
 import pygame
 
-from settings import Settings
+import settings
+
+
+class Character:
+    def __init__(self, control: settings.UserControlSettings):
+        self.surface = pygame.Surface((100, 100))
+        self.surface.fill((250, 50, 50))
+        self.rect = self.surface.get_rect()
+        self.rect.center = (300, 300)
+        self._control = control
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[self._control.move_right] and keys[self._control.move_up]:
+            self.rect.x += 1
+            self.rect.y -= 1
+        elif keys[self._control.move_right] and keys[self._control.move_down]:
+            self.rect.x += 1
+            self.rect.y += 1
+        elif keys[self._control.move_left] and keys[self._control.move_up]:
+            self.rect.x -= 1
+            self.rect.y -= 1
+        elif keys[self._control.move_left] and keys[self._control.move_down]:
+            self.rect.x -= 1
+            self.rect.y += 1
+        elif keys[self._control.move_right]:
+            self.rect.x += 1
+        elif keys[self._control.move_left]:
+            self.rect.x -= 1
+        elif keys[self._control.move_up]:
+            self.rect.y -= 1
+        elif keys[self._control.move_down]:
+            self.rect.y += 1
+
+    def draw(self, screen):
+        screen.blit(self.surface, self.rect)
 
 
 class Window(ABC):
@@ -172,7 +207,7 @@ class RowSetting(Control):
 
 
 class SettingWindow(Window):
-    def __init__(self, caption, size, settings: Settings):
+    def __init__(self, caption, size, settings: settings.Settings):
         super().__init__(caption, size)
         self._controls: list[RowSetting] = []
         self._settings = settings
