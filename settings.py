2e2c65ce9ca6962d2036ad68e69fe4bee139cf1c
PavelAltynnikov <Robenzon11@gmail.com>
2023-01-03 20:40:24 +0600

    change settings logick

--- settings.py
+++ settings.py
@@ -1,27 +1,33 @@
+from abc import ABC
 import json
 
-import pygame
+
+class Setting:
+    def __init__(self, value: int):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
 
 
-def get_ui_settings():
+class ControlSettings(ABC):
+    def __init__(self, right: Setting, left: Setting, up: Setting, down: Setting):
+        self.right = right
+        self.left = left
+        self.up = up
+        self.down = down
+
+    def save(self):
+        with open('settings.json', 'w') as file:
+            file.write(json.dumps(self.__dict__, default=lambda o: o.__dict__))
+
+
+class PygameKeyboardControlSettings(ControlSettings):
+    def __init__(self, right, left, up, down):
+        super().__init__(right, left, up, down)
+
+
+def get_ui_settings(settings_type: type[ControlSettings]) -> ControlSettings:
     with open('settings.json', 'r') as file:
-        return json.loads(
-            ''.join(file.readlines()),
-            object_hook=lambda d: Settings(**d)
-        )
-
-
-class UserControlSettings:
-    def __init__(self):
-        self.move_right = pygame.K_RIGHT
-        self.move_up = pygame.K_UP
-        self.move_left = pygame.K_LEFT
-        self.move_down = pygame.K_DOWN
-
-
-class Settings:
-    def __init__(self, right_key, left_key, up_key, down_key):
-        self.right_key = right_key
-        self.left_key = left_key
-        self.up_key = up_key
-        self.down_key = down_key
+        data = json.loads(''.join(file.readlines()))
+        return settings_type(**{k: Setting(v["value"]) for k, v in data.items()})
