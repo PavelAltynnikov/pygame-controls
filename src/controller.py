fae194dc7a9926db9a49f10646b8fcc284d4a821
PavelAltynnikov <Robenzon11@gmail.com>
2023-01-19 02:43:24 +0500

    update Mover class

--- src/controller.py
+++ src/controller.py
@@ -109,20 +109,24 @@ class Mover:
         self._controller = controller
 
     def move_character(self, character: model.Character):
-        keys = pygame.key.get_pressed()
-        speed = 1
-
+        speed = 0
         character.move_to(
             model.Point(
-                x=self._controller.get_new_x(
-                    keys,
-                    character.location.x,
-                    speed
-                ),
-                y=self._controller.get_new_y(
-                    keys,
-                    character.location.y,
-                    speed
-                )
+                x=self._get_new_x(character.location.x, speed),
+                y=self._get_new_y(character.location.y, speed)
             )
         )
+
+    def _get_new_x(self, start_x: float, speed: float) -> float:
+        if self._controller.move_right.activated:
+            return start_x + self._controller.move_right.value + speed
+        if self._controller.move_left.activated:
+            return start_x - abs(self._controller.move_left.value) - speed
+        return start_x
+
+    def _get_new_y(self, start_y: float, speed: float) -> float:
+        if self._controller.move_up.activated:
+            return start_y - abs(self._controller.move_up.value) - speed
+        if self._controller.move_down.activated:
+            return start_y + self._controller.move_down.value + speed
+        return start_y
