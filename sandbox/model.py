07cc653c493eba68390a1226a9c441a1677ceda7
PavelAltynnikov <Robenzon11@gmail.com>
2023-01-31 01:21:57 +0500

    add change location methods into Character

--- sandbox/model.py
+++ sandbox/model.py
@@ -7,10 +7,20 @@ class Point:
 class Character:
     def __init__(self, start_point: Point):
         self._location = start_point
+        self.is_jumping = False
 
     @property
     def location(self) -> Point:
         return self._location
 
     def move_to(self, point: Point) -> None:
+        """Кардинально изменить точку расположения объекта"""
         self._location = point
+
+    def change_x(self, dx: float) -> None:
+        """Изменить часть текущей координаты"""
+        self._location.x += dx
+
+    def change_y(self, dy: float) -> None:
+        """Изменить часть текущей координаты"""
+        self._location.y += dy
